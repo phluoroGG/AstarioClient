9973efe52c6b8de895d2a10239a4a490c1fbae9d
phluoroGG <phluoro@gmail.com>
2023-05-22 17:34:41 +0300

    Initialize project

---
+++ app/src/main/java/com/tp31/astarioclient/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.tp31.astarioclient.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
